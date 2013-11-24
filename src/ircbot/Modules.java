b6ef84704d6b01635de0e9d6534acaedebd911db
Miro Nieminen <leonarven@gmail.com>
2013-11-24 14:55:23 +0200

    Modules.unload

--- src/ircbot/Modules.java
+++ src/ircbot/Modules.java
@@ -4,6 +4,7 @@ import ircbot.modules.Module;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Modules {
 
@@ -24,6 +25,20 @@ public class Modules {
 		for(String replyCommand : module.ircReplyListeners)
 			if (ircReplyListenerMap.containsKey(replyCommand))
 				ircReplyListenerMap.get(replyCommand).add(module.name);
+	}
+	public static void unload(String moduleName) {
+		if (!moduleMap.containsKey(moduleName)) return;
+		
+		moduleMap.remove(moduleName);
+		
+		for(Map.Entry<String, String> entry : commandMap.entrySet())
+			if (entry.getValue().equals(moduleName)) commandMap.remove(entry.getKey());
+			
+		// TODO for-loop for ircReplyListenerMap
+		for(Map.Entry<String, ArrayList<String>> listEntry : ircReplyListenerMap.entrySet())
+			for(int i = 0; i < listEntry.getValue().size(); i++)
+				if (listEntry.getValue().get(i).equals(moduleName))
+					ircReplyListenerMap.get(listEntry.getKey()).remove(i);
 	}
 	
 	public static Module get(String moduleName) {
